6bb9d9db48e44c7c7378820106835265a800b294
Jason <beefers331@gmail.com>
2021-06-12 14:31:27 -0600

    Linux stacktrace working

--- src/hottie/linux.nim
+++ src/hottie/linux.nim
@@ -21,6 +21,8 @@ proc getThreadIds*(pid: int): seq[int] =
       fetchStackAddrs(tid)
     except: discard
 
+proc toRel(address: uint64, pid: int): uint64 = address - startOffsets[pid]
+
 proc sample*(
   cpuSamples: var int,
   cpuHotAddresses: var Table[int64, int],
@@ -38,9 +40,43 @@ proc sample*(
   wait(nil)
   getRegs(threadId, addr regs)
 
-  detach(threadId)
-  let rip = regs.rip.int - startOffsets[threadId].int
+  var stackTrace: string
+  if stacks:
+    var prevFun = ""
+    block:
+      let startAddress = regs.rsp
+      var i = 0
+      let dl = dumpLine.addressToDumpLine(regs.rip.toRel(threadId))
+      prevFun = dl.text.split(" @ ")[0]
+      stackTrace.add prevFun.split("__", 1)[0]
+      stackTrace.add "<"
+      while i < 10_000:
+        var value = threadId.getData((startAddress + i.culong).clong).uint64
+        let dl = dumpLine.addressToDumpLine(value.toRel(threadId))
+        if "stdlib_ioInit000" in dl.text or "NimMainModule" in dl.text:
+          break
+        if dl.text != "":
+          let thisFun = dl.text.split(" @ ")[0]
+          let canCall = prevFun in callGraph[thisFun]
+          if canCall:
+            if prevFun == thisFun:
+              if not stackTrace.endsWith("*"):
+                stackTrace[^1] = '*'
+            else:
+              prevFun = thisFun
+              stackTrace.add thisFun.split("__", 1)[0]
+              stackTrace.add "<"
+        i += 8
 
+
+  detach(threadId)
+  let rip = regs.rip.toRel(threadId).int
+  
   if cpuHotAddresses.hasKeyOrPut(rip, 1):
     inc cpuHotAddresses[rip] 
   inc cpuSamples
+  if stacks:
+    if stackTrace notin cpuHotStacks:
+      cpuHotStacks[stackTrace] = 1
+    else:
+      cpuHotStacks[stackTrace] += 1
